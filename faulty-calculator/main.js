4e539a0e24832dee9a0c1ef14cb50226b6985c81
kushika0 <kushika1404@gmail.com>
2025-09-28 18:07:30 +0530

    new

--- faulty-calculator/main.js
+++ faulty-calculator/main.js
@@ -1,1 +1,24 @@
-let 
+//This is the faulty calculator which gives incorrect output  (10% times )
+
+let fault= Math.random()
+
+
+
+let a=prompt("Fisrt number")
+let b= prompt("Second Number")
+let c=prompt("enter the operation")
+
+let obj={
+    "+":"-",
+    "-":"*",
+    "*":"/",
+    "/":"+"
+}
+
+if (fault>0.1){
+    alert(`The result is ${eval(`${a} ${c} ${b} `)}`)
+}
+else{
+    c=obj[c]
+   alert(`The result is ${eval(`${a} ${c} ${b} `)}`)
+}
